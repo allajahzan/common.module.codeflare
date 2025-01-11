2e47e04d9772572e35718d602c9edf293dea2659
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-11 16:41:53 +0530

    feat: isToken expired function

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -8,12 +8,30 @@ export interface JwtPayloadType {
     role: string;
 }
 
+// Check Token validity
+const isTokenExpired = (token: string) => {
+    try {
+        const decoded = jwt.decode(token) as JwtPayload | null;
+        if (!decoded || !decoded.exp) {
+            throw new ForbiddenError();
+        }
+
+        const currentTime = Math.floor(Date.now() / 1000);
+        return decoded.exp < currentTime;
+    } catch (err) {
+        throw new Error("Internal Server Error");
+    }
+};
 
 // Verify Access Token
 const verifyAccessToken = (req: Request, res: Response, next: NextFunction) => {
     try {
         const accessToken = req.headers["authorization"]?.split(" ")[1];
         if (!accessToken) throw new ForbiddenError();
+
+        if (isTokenExpired(accessToken)) {
+            throw new UnauthorizedError("Token Expired");
+        }
 
         const payload = jwt.verify(
             accessToken,
