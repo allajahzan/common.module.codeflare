0afffa8dd6dc7170064dfd0f25baef8c931dd292
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-13 13:45:25 +0530

    feat: mongodb connection with retry mechanism

---
+++ src/config/mongodb.connection.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+export class MongodbConnection {
+    private attempt: number = 1;
+    private retries: number = 5;
+    private delay: number = 5000;
+    private url: string;
+
+    /**
+     * Creates an instance of MongodbConnection.
+     * @param {string} url The mongodb connection string
+     */
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    /**connect to mongodb */
+    async connect() {
+        await mongoose.connect(this.url, {
+            connectTimeoutMS: 1000,
+            serverSelectionTimeoutMS: 5000,
+        });
+        console.log("Connected to MongoDB");
+    }
+
+    /**retry connection */
+    async retryConnection() {
+        while (this.attempt <= this.retries) {
+            try {
+                await this.connect();
+                break;
+            } catch (err: any) {
+                console.log(
+                    `Connection attempt ${this.attempt} failed : ${err.message}. Retrying in ${this.delay}ms...`
+                );
+                await new Promise((res) => setTimeout(res, this.delay));
+                this.attempt++;
+            }
+        }
+        throw new Error("Failed to connect to MongoDB after retries.");
+    }
+}
